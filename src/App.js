82421b24a7f8b78d9359b8464e6e529736d36e4b
Muhammad Ziyad <ziyadhilalluddin@gmail.com>
2023-07-03 00:46:08 +0800

    login auth function

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import React, {useState}from 'react'
-import Login from './Login'
-import Signup from './Signup'
+import React, { useState } from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Login from './Login';
+import Signup from './Signup';
 import Profile from './Profile';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
-  const [user, setUser] = useState({});
+  const [user, setUser] = useState(null);
+
+  const handleLogin = (userData) => {
+    setUser(userData);
+  };
+
+  const handleLogout = () => {
+    setUser(null);
+  };
+
+  const ProtectedRoute = ({ element: Component, ...rest }) => {
+    return user ? <Component user={user} handleLogout={handleLogout} {...rest} /> : <Navigate to="/" />;
+  };
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/'element={<Login />}></Route>
-        <Route path='/signup'element={<Signup />}></Route> 
-        <Route path="/profile" element={<Profile />}state={{}}/>
-
+        <Route path="/" element={<Login handleLogin={handleLogin} />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/profile" element={<ProtectedRoute element={Profile} />} />
       </Routes>
     </BrowserRouter>
-
-
-
-
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Login.js
+++ src/Login.js
@@ -3,15 +3,15 @@ import { Link, useNavigate } from 'react-router-dom';
 import Validation from './LoginValidation';
 import axios from 'axios';
 import './Profile.css';
-function Login() {
+
+function Login({ handleLogin }) {
     const [values, setValues] = useState({
         email: '',
         password: ''
     });
     const navigate = useNavigate();
     const [errors, setErrors] = useState({});
-    const [backendError, setBackendError] = useState([]);
-
+    const [backendError, setBackendError] = useState('');
 
     const handleInput = (event) => {
         setValues((prev) => ({ ...prev, [event.target.name]: event.target.value }));
@@ -24,24 +24,25 @@ function Login() {
 
     useEffect(() => {
         if (errors.email === '' && errors.password === '') {
-            axios.post('http://localhost:8081/login', values)
-                .then(res => {
+            axios
+                .post('http://localhost:8081/login', values)
+                .then((res) => {
                     if (res.data.errors) {
                         setBackendError(res.data.errors);
                     } else {
-                        setBackendError([]);
+                        setBackendError('');
                         if (res.data.success === true) {
                             console.log(res.data);
+                            handleLogin(res.data.user);
                             navigate('/profile', { state: { user: res.data.user } });
                         } else {
-                            alert("No record existed");
+                            alert('No record existed');
                         }
                     }
                 })
-                .catch(err => console.log(err));
-
+                .catch((err) => console.log(err));
         }
-    }, [errors, navigate, values]);
+    }, [errors, handleLogin, navigate, values]);
 
     return (
         <div className="d-flex justify-content-center align-items-center vh-100 gradient-custom">
@@ -80,9 +81,13 @@ function Login() {
                         <strong>Login</strong>
                     </button>{' '}
                     <p></p>
-                    <Link to="/signup" className="btn btn-default border w-100 bg-light rounded-0 text-decoration-none">
+                    <Link
+                        to="/signup"
+                        className="btn btn-default border w-100 bg-light rounded-0 text-decoration-none"
+                    >
                         Create Account
                     </Link>
+                    {backendError && <p className="text-danger mt-3">{backendError}</p>}
                 </form>
             </div>
         </div>
